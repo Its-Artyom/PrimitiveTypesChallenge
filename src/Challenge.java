7a109ada4c7267cff42c292899624e5eadbd2903
Artyom Pionov <pionovartem11@gmail.com>
2022-10-02 21:13:40 +0100

    Second Change

--- src/Challenge.java
+++ src/Challenge.java
@@ -1,10 +1,10 @@
 public class Challenge {
     public static void main(String[] args){
-        byte variable1 = 120;
-        short variable2 = 1776;
-        int variable3 = 326_785;
-        long variable4 = 50000L + 10L*(long)(variable1 + variable2 + variable3);
+        byte byteValue = 120;
+        short shortValue = 1776;
+        int intValue = 326_785;
+        long longValue = 50000L + 10L*(long)(byteValue + shortValue + intValue);
 
-        System.out.println(variable4);
+        System.out.println(longValue);
     }
 }
